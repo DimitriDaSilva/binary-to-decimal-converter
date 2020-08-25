4abc08bca17a53f2d5cc1e1c61caf276a9199dfe
Dimitri <dimitri.gomes.da.silva@gmail.com>
2020-08-25 16:39:43 +0100

    Added random binary number on load

--- main.js
+++ main.js
@@ -1,60 +1,66 @@
-// Set global array with the initial binary number
-let binaryNumbers = [0, 1, 0, 1, 0, 1, 0, 1];
 let binaryTable = [128, 64, 32, 16, 8, 4, 2, 1];
 
-// Set a loop listening to all light-bulbs
-let elements = document.getElementsByClassName("binary__item__light-bulb");
-for (let i = 0; i < elements.length; i++) {
-  elements[i].addEventListener("click", update);
+// Set the array of random binary numbers onload
+let binaryNumbers = Array(8).fill(undefined);
+binaryNumbers = binaryNumbers.map(() => Math.round(Math.random()));
+
+// Load page according to the array
+setIconAndNumbers();
+setDecimal();
+
+function setIconAndNumbers() {
+  let index = 0;
+  binaryNumbers.forEach((number) => {
+    // Set icon
+    let idImg = `light-bulb${index}`;
+    let elImg = document.getElementById(idImg);
+    if (number) {
+      elImg.src = "./img/light-bulb-on.png";
+    } else {
+      elImg.src = "./img/light-bulb-off.png";
+    }
+
+    // Set number
+    let idP = `binary-digit${index}`;
+    let elP = document.getElementById(idP);
+    elP.textContent = number;
+
+    // Update index to continue looping
+    index++;
+  });
 }
 
-function update() {
-  let index = checkIndex(this.id)[0];
-  changeIcon(this);
-  updateDigit(index);
-  updateDecimal();
-}
-
-function changeIcon(obj) {
-  let regex = /on/;
-  let imageSource = obj.src;
-
-  if (regex.test(imageSource)) {
-    obj.src = imageSource.replace("on", "off");
-  } else {
-    obj.src = imageSource.replace("off", "on");
-  }
-}
-
-function checkIndex(id) {
-  return id.match(/.$/);
-}
-
-function updateDigit(index) {
-  let str = "binary-digit";
-
-  // Get proper id string
-  str = str.split("");
-  str.push(index);
-  str = str.join("");
-
-  // Change number displayed
-  let el = document.getElementById(str);
-  if (el.innerHTML == 1) {
-    el.innerHTML = 0;
-  } else {
-    el.innerHTML = 1;
-  }
-
-  // Update array with binary number
-  binaryNumbers[index] = parseInt(el.innerHTML, 2);
-}
-
-function updateDecimal() {
+function setDecimal() {
   let sum = 0;
 
   for (let i = 0; i < binaryTable.length; i++) {
     sum += binaryNumbers[i] * binaryTable[i];
   }
   document.getElementById("decimal").innerHTML = sum;
+}
+
+// Set a loop listening to all light-bulbs so that it updates onclick
+let elements = document.getElementsByClassName("binary__item__light-bulb");
+for (let i = 0; i < elements.length; i++) {
+  elements[i].addEventListener("click", update);
+}
+
+// Function that updates the array of ninary numbers then reruns the load
+function update() {
+  // Extract from the object id the corresponding index of the array
+  let index = checkIndex(this.id)[0];
+
+  // Update the array based on the existing value of the array
+  if (binaryNumbers[index]) {
+    binaryNumbers[index] = 0;
+  } else {
+    binaryNumbers[index] = 1;
+  }
+
+  setIconAndNumbers();
+  setDecimal();
+}
+
+function checkIndex(id) {
+  return id.match(/.$/);
 }
